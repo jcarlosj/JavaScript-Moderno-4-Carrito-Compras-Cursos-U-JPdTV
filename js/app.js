64d82ca6218af2613216beebe3e33806da61a6a9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-26 15:57:29 -0500

    Crea un objeto, obtiene los datos del curso y lo agrega al DOM usando un 'String Template'

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 
 /* Obtenemos los elementos del DOM donde vamos a agregar los cursos */   
 const carritoCompras = document .getElementById( 'carrito' ),          // Elemento Padre de 'lista-cursos'
-      listaCursos    = document .getElementById( 'lista-cursos' );      // Elemento hijo de 'carrito'
+      listaCursos    = document .getElementById( 'lista-cursos' ),     // Elemento hijo de 'carrito'
+      cursosEnElCarrito = document .querySelector( '#lista-carrito tbody' );    // Elemento donde se van a insertar los cursos al carrito
 
 /* Escucha los eventos */
 cargarEventos();                                                                                                                                                                                                                                                                                    
@@ -23,11 +24,9 @@ function comprarCurso( e ) {
 
         // Delegation: Valida si el elemento al que se le dió click contiene la clase 'agregar carrito'
         if( e .target .classList .contains( 'agregar-carrito' ) ) {
-
             const curso = e .target .parentElement .parentElement;      // Card del Curso
 
             leerDatosCurso( curso );
-            console .log( 'Elemento Card del Curso: ', curso );
         }
 
     console .groupEnd();
@@ -35,5 +34,35 @@ function comprarCurso( e ) {
 
 // Función que lee los datos del curso
 function leerDatosCurso( curso ) {
-    console .log( 'leerDatosCurso ', curso );
+    // Creamos un objeto con todos los datos del curso
+    const infoCurso = {
+        imagen: curso .querySelector( 'img' ) .src,                     // Obtenemos el URL de la imagen
+        titulo: curso .querySelector( 'h4' ) .textContent,              // Obtenemos el nombre del curso
+        precio: curso .querySelector( '.precio span' ) .textContent,    // Obtenemos el precio del curso
+        id:     curso .querySelector( 'a' ) .getAttribute( 'data-id' )  // Obtenemos el id del curso (valor personalizado)
+    }
+
+    console .log( 'leerDatosCurso ', infoCurso );
+
+    insertarAlCarrito( infoCurso );
+}
+
+// Functión que muestra el curso seleccionado en el carrito
+function insertarAlCarrito( curso ) {
+    const filaTabla = document .createElement( 'tr' );          // Crea elemento HTML 'tr'
+
+    // Creamos un 'String Template' que queremos que se despliegue dentro del elemento creado 'tr'
+    filaTabla .innerHTML = `
+        <td>
+            <img src="${ curso .imagen }" width="120" />
+        </td>
+        <td>${ curso .titulo }</td>
+        <td>${ curso .precio }</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${ curso .id }" >x</a>
+        </td>
+    `;
+
+    // Insertamos el 'String Template' del curso en el DOM
+    cursosEnElCarrito .appendChild( filaTabla );
 }
